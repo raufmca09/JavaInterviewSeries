6257174568456426b1a34475c5a74ee53799cca1
Abdulrouf Lathori <abdulrouf.lathori@gmail.com>
2026-08-26 18:49:57 +0530

    Updated employee java file

---
+++ src/main/java/org/collection/Employee.java
@@ -0,0 +1,15 @@
+package org.collection;
+
+public class Employee {
+
+    String name;
+    int age;
+    String dept;
+
+    Employee(int age, String name, String dept){
+        this.name = name;
+        this.age = age;
+        this.dept = dept;
+    }
+
+}
