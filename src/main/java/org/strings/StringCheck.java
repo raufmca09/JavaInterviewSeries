aae64f3ad0384e8d6b101349daf2b632a1a3bcfe
Abdulrouf Lathori <abdulrouf.lathori@gmail.com>
2026-08-26 18:16:17 +0530

    String program added

---
+++ src/main/java/org/strings/StringCheck.java
@@ -0,0 +1,39 @@
+package org.strings;
+
+public class StringCheck {
+
+    public static void main(String[] args ){
+
+        String x = "abc";
+        String y = "abc";
+
+        System.out.println(x.concat(y));
+
+        System.out.println(x);
+
+        String result = Captialize("hello am from bangalore");
+        System.out.println("After capitalize -> " + result);
+
+        //swapping to strings without 3rd variable
+        String a = "Hello";String b = "World";
+        a= a+b;//HelloWorld
+        b= a.substring(0,a.length()-b.length());
+        a = a.substring(b.length());
+
+        System.out.println("A -> " + a + " B -> " + b);
+
+    }
+
+    public static String Captialize(String str){
+        String result = "";
+
+        String[] words = str.split("\\s");
+
+        for(String word: words){
+            String first = word.substring(0,1).toUpperCase();
+            String last = word.substring(1).toLowerCase();
+            result = result+first+last;
+        }
+        return result.trim();
+    }
+}
